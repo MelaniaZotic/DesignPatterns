6d45d02a4135f1e0def9dfcd2afe716997c4e0b8
MelaniaZotic <82153993+MelaniaZotic@users.noreply.github.com>
2022-03-30 16:52:16 +0300

    fruit

---
+++ homework/FactoryMethod/src/Fruits.java
@@ -0,0 +1,3 @@
+public interface Fruits {
+    void fruitEaten();
+}
---
+++ homework/FactoryMethod/src/Melon.java
@@ -0,0 +1,6 @@
+public class Melon implements Fruits{
+    @Override
+    public void fruitEaten() {
+        System.out.println("A Melon was eaten");
+    }
+}
