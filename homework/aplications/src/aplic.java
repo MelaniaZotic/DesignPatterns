2c52f9a4d5b31a56308e53b72e3e57450f9fc88c
MelaniaZotic <82153993+MelaniaZotic@users.noreply.github.com>
2022-04-13 12:47:09 +0300

    homework3

---
+++ homework/aplications/src/aplic.java
@@ -0,0 +1,24 @@
+public static void main(String args[]) {
+        Integer intObj=Integer.valueOf(args[args.length-1]);
+        int i = intObj.intValue();
+
+        if(args.length > 1)
+        System.out.println(i);
+        if(args.length > 0)
+        System.out.println(i - 1);
+        else
+        System.out.println(i - 2);
+        }
+        }
+public static void main(String args[]) {
+        Integer intObj=Integer.valueOf(args[args.length-1]);
+        int i = intObj.intValue();
+
+        if(args.length > 1)
+        System.out.println(i);
+        if(args.length > 0)
+        System.out.println(i - 1);
+        else
+        System.out.println(i - 2);
+        }
+        }
